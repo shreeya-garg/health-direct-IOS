21d665f661867c246bf43cc9c2f31d592def0817
FemK157 <fskovoor@gmail.com>
2024-09-28 18:54:43 -0400

    tweaked homescreen

--- health direct/health direct/ContentView.swift
+++ health direct/health direct/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     @State private var dailyWaterIntake: String = ""
     @State private var navigateToConfirmation: Bool = false
     
-    var sexes = ["Sex", "Female", "Male"]
+    var sexes = ["Sex", "Female", "Male", "Other"]
     
     var body: some View {
         NavigationView {
@@ -69,7 +69,7 @@ struct ContentView: View {
                     .padding(.bottom, 10)
                     
                     TextField(
-                        "Daily Water Intake",
+                        "Daily Water Intake (cups/day)",
                         text: $dailyWaterIntake
                     )
                     .cornerRadius(15)
@@ -127,32 +127,90 @@ func format_information(medications: String, med_conditions: String, age: String
 }
 
 
-struct SubmitConfirmationView: View {
+struct HomescreenView: View {
     let speechManager = SpeechManager()
     
     var body: some View {
         VStack {
-            Text("Thank You!")
-                .font(.largeTitle)
-                .padding()
-            Text("Your information has been submitted.")
-                .font(.subheadline)
-                .padding()
-        }
-        .navigationTitle("Confirmation") // Title for the new view
-        
-        Button {
-            speechManager.speakText("Thank you for submitting your information. Please follow the instructions accordingly.")
-            print("PRESSED")
-        } label: {
-            Image("SubmitButton")
+            HStack {
+                Image("HealthDirectLogo")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100, height: 50)
-                .shadow(radius: 5)
+                .scaledToFit() 
+                .frame(width: 100, height: 100)
+
+                Spacer()
+            }
+            
+            HStack {
+                Image(systemName: "eyedropper.halffull")
+                Text("Blood Pressure: \(systolic)/\(diastolic)")
+            }
+
+            HStack {
+                Image(systemName: "lungs")
+                Text("Respiratory Rate: \(rate)")
+            }
+            
+            HStack {
+                Image(systemName: "bandage.fill")
+                Text("Medications: \(medications)")
+            }
+
+            HStack {
+                Image(systemName: "plus.square.fill")
+                Text("Pre-existing Medical Conditions: \(med_conditions)")
+            }
+
+            HStack {
+                Image(systemName: "drop.triangle.fill")
+                Text("Daily Water Intake: \(dailyWaterIntake)")
+            }
+           
+            HStack {
+                Image(systemName: "heart.person.fill")
+                Text("HK Biological Sex: \(HKObjectType.characteristicType(forIdentifier: .biologicalSex))")
+            }
+            
+            HStack {
+                Image(systemName: "heart.person.fill")
+                Text("Inputted Sex: \(sex)")
+            }
+
+            HStack {
+                Image(systemName: "eyeglasses")
+                Text("Age: \(age)")
+            }
+            
         }
+        .bold
+        .padding()
+        .foreground(Color(red: 209, blue: 22, green: 8))
+        .imageScale(.small)
+        
+        //VStack {
+        //    Text("Thank You!")
+        //        .font(.largeTitle)
+        //        .padding()
+        //    Text("Your information has been submitted.")
+        //        .font(.subheadline)
+        //        .padding()
+        //}
+        //.navigationTitle("Confirmation") // Title for the new view
+        //
+        //Button {
+        //    speechManager.speakText("Thank you for submitting your information. Please follow the instructions accordingly.")
+        //    print("PRESSED")
+        //} label: {
+        //    Image("SubmitButton")
+        //        .resizable()
+        //        .aspectRatio(contentMode: .fit)
+        //        .frame(width: 100, height: 50)
+        //        .shadow(radius: 5)
+        //}
     }
 }
+
+
 
 //func vocalizer() {
 //    // Usage
