78b00865033ed75531c77c72a3b3eb249231a285
Aparupa <brahmapa000@hsestudents.org>
2024-09-29 04:11:00 -0400

    now fine tuned model

--- health direct/health direct/integrateModel.swift
+++ health direct/health direct/integrateModel.swift
@@ -15,9 +15,10 @@ class MedicalAssistant {
     // Method to generate a response with variable user input
     func generateMedicalResponse(userHistory: String, biometricData: String, context: String) async throws -> String {
         let prompt = """
-        Serve as a medical instruction provider. When a medical emergency occurs, you will provide clear, distinct, and concise steps that bystanders should take to help the situation. You will be given personal userHistory, current biometricData, and potentially additional context about the situation or symptoms. Based on this, you must attempt to identify what might be going wrong and provide a few steps that surrounding individuals, who are not medical professionals, should take to help that person. The userHistory is \(userHistory). The biometric data is \(biometricData). The context is \(context).
+            userHistory: \(userHistory)
+            biometric data: \(biometricData).
+            context:\(context)
         """
-        
         let response = try await generativeModel.generateContent(prompt)
         
         if let text = response.text {
