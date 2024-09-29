a5908fa6d3c563dcd5a0a966c467bd4e8704fffc
sgarg330 <sgarg330@gatech.edu>
2024-09-29 04:40:37 -0400

    fine tuned model working

--- health direct/health direct/integrateModel.swift
+++ health direct/health direct/integrateModel.swift
@@ -7,7 +7,7 @@ class MedicalAssistant {
     // Initialize the model with the API key that stays the same
     init() {
         self.generativeModel = GenerativeModel(
-            name: "gemini-1.5-flash",
+            name: "tunedModels/medical-response-prompt-kvnavv2r69ea",
             apiKey: "AIzaSyCNG5vSVGUiXco8hEI4qsX49xs1uBEsStU"
         )
     }
@@ -15,13 +15,15 @@ class MedicalAssistant {
     // Method to generate a response with variable user input
     func generateMedicalResponse(userHistory: String, biometricData: String, context: String) async throws -> String {
         let prompt = """
+        You are serving as a medical instruction provider. When a medical emergency occurs, you will provide clear, distinct, and concise steps that bystanders should take to help with the situation. Based on the information given below, identify what might be going on and then provide a few clear steps that surrounding indivuduals should take to help that person while waiting for medical personel to arrive.
             userHistory: \(userHistory)
-            biometric data: \(biometricData).
+            biometric data: \(biometricData)
             context:\(context)
         """
         let response = try await generativeModel.generateContent(prompt)
         
         if let text = response.text {
+            print(text)
             return text
         } else {
             return "No response from the model."
